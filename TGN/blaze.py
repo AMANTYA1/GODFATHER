98a86bb2d762b96c1931d978f1a98e680bdf4efc
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2022-11-04 23:14:08 +0530

    Create blaze.py

---
+++ TGN/blaze.py
@@ -0,0 +1,43 @@
+#ORIGINAL GUY NO KANG
+
+import asyncio
+import json
+import math
+import os
+import random
+import re
+import shlex
+import time
+from pymongo import MongoClient
+from requests import Request, Session
+from random import choice, randint
+from typing import Tuple
+from urllib.parse import quote
+
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
